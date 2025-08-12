f4d144eecc6e253405e96023c4cbbce23e537f84
Nando-Barros23 <fernandobarrosc16@gmail.com>
2025-08-11 21:18:08 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
+// Cole este código completo no seu arquivo auth.js
+
 document.addEventListener('DOMContentLoaded', () => {
-    // CORREÇÃO: A conexão com o Supabase foi movida para DENTRO deste bloco.
     const { createClient } = supabase;
     const SUPABASE_URL = 'https://zslokbeazldiwmblahps.supabase.co';
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzbG9rYmVhemxkaXdtYmxhaHBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NDA2NDcsImV4cCI6MjA3MDAxNjY0N30.UfTi-SBzIa9Wn_uEnQiW5PAiTECSVimnGGVJ1IFABDQ';
