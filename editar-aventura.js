c6bd737aab40bedc5b243062817120ea00998b2c
Nando-Barros23 <fernandobarrosc16@gmail.com>
2025-08-19 14:10:26 -0300

    Update editar-aventura.js

--- editar-aventura.js
+++ editar-aventura.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     // 1. INICIALIZAÇÃO
     const { createClient } = supabase;
     const SUPABASE_URL = 'https://zslokbeazldiwmblahps.supabase.co';
-    const SUPABASE_ANON_KEY = 'SUA_CHAVE_ANON_PUBLICA_VEM_AQUI'; // <-- COLE SUA CHAVE AQUI
+    const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzbG9rYmVhemxkaXdtYmxhaHBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0NDA2NDcsImV4cCI6MjA3MDAxNjY0N30.UfTi-SBzIa9Wn_uEnQiW5PAiTECSVimnGGVJ1IFABDQ'; // <-- COLE SUA CHAVE AQUI
     const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
     const adventureForm = document.getElementById('adventure-form');
